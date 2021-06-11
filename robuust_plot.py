04eb7c2573ffe6b5c417796dd260ea0f7427a414
Tingyi <tiwanyan@gmail.com>
2021-06-11 13:19:37 -0400

    correction

---
+++ robuust_plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+size = [345]
+
+train_lr = [0.695]
+
+train_rf = [0.711]
+
+train_svm = [0.690]
+
+train_xgb = [0.684]
+
+FL_random = [0.766]
+
+FL_ATT
