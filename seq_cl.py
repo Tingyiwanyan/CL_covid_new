8bd9d839ba51db7bfb8cb89b48daa51d29f0f8f9
Tingyi <tiwanyan@gmail.com>
2021-06-11 16:30:23 -0400

    correction

--- seq_cl.py
+++ seq_cl.py
@@ -46,7 +46,7 @@ class seq_cl():
         self.batch_size = 64
         self.vital_length = self.read_d.vital_length
         self.lab_length = self.read_d.lab_length
-        self.epoch = 5
+        self.epoch = 3
         self.epoch_pre = 10
         self.gamma = 2
         self.tau = 1
@@ -604,7 +604,7 @@ class seq_cl():
             for j in range(self.iteration):
                 #print(j)
                 self.aquire_batch_data_cl(j*self.batch_size, self.train_data, self.batch_size,self.read_d.time_sequence)
-                self.err_ = self.sess.run([self.focal_loss, self.train_step_ce,self.logit_sig],
+                self.err_ = self.sess.run([self.focal_loss, self.train_step_fl,self.logit_sig],
                                           feed_dict={self.input_x: self.one_batch_data,
                                                      self.input_y_logit: self.one_batch_logit_dp,#})
                                                      self.input_x_pos:self.one_batch_data_pos,
