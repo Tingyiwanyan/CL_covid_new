caf2721bdc0bef746b216cca9c020bfb28303b55
Tingyi <tiwanyan@gmail.com>
2021-06-11 12:05:07 -0400

    correction

--- calibration.py
+++ calibration.py
@@ -3,6 +3,9 @@ import numpy as np
 class calibrate():
     def __init__(self, read_d, seq_cl):
         self.read_d = read_d
+        all_data = list(self.read_d.dic_patient.keys())
+        self.test_data = all_data[3891:]
+        self.len_test = len(self.test_data)
         self.seq_cl = seq_cl
 
     def fit(self, x, y):
@@ -10,4 +13,5 @@ class calibrate():
         return self
 
     def decision_function(self, X):
-        return  self.seq_cl.sess.run(self.seq_cl.logit_sig, feed_dict={self.seq_cl.input_x: X})
+        self.seq_cl.aquire_batch_data(0, self.test_data, self.len_test, self.read_d.time_sequence)
+        return  self.seq_cl.sess.run(self.seq_cl.logit_sig, feed_dict={self.seq_cl.input_x: self.seq_cl.one_batch_data})
