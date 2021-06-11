a06d5a03be690dbb69b2175ff4bafa7bd9c06fbb
Tingyi <tiwanyan@gmail.com>
2021-06-11 14:00:58 -0400

    correction

--- robuust_plot.py
+++ robuust_plot.py
@@ -11,7 +11,7 @@ train_svm = [0.690,0.719,0.751,0.751,0.760]
 
 train_xgb = [0.684,0.689,0.729,0.749,0.758]
 
-CE=[0.710]
+CE=[0.668, 0.672,0.694,0.701,0.710]
 
 FL=[0.777]
 
@@ -23,20 +23,12 @@ FL_ATT = [0.774,0.803,0.815,0.827]
 
 plt.xlabel("Traing Size")
 plt.ylabel("AUC")
-plt.title("Prediction Performance on Various Sample Sizes in the same Test Set", fontsize=14)
-#plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-x = [0.0, 1.0]
+plt.title("Prediction Performance", fontsize=14)
+plt.xlim(300, 3500)
+plt.ylim(0.5, 0.9)
+#x = [0.0, 1.0]
 
-plt.plot(recall_total_lr, precision_total_lr, color='green', linewidth=2, linestyle='dashed',label='LR(AUPRC=0.524)')
-plt.plot(recall_total_rf, precision_total_rf,color='blue',linestyle='dashed',linewidth=2,label='RF(AUPRC=0.593)')
-plt.plot(recall_total_svm,precision_total_svm,color='violet',linestyle='dashed',linewidth=2,label='SVM(AUPRC=0.560)')
-plt.plot(recall_total_xgb,precision_total_xgb,color='red',linestyle='dashed',linewidth=2,label='XGB(AUPRC=0.563)')
-#plt.plot(fp_total_ce,tp_total_ce,color='indigo',linestyle='dashed',linewidth=2,label='CE(AUC=0.743)')
-plt.plot(recall_total_fl,precision_total_fl,color='orange',linestyle='dashed',linewidth=2,label='FL(AUPRC=0.604)')
-plt.plot(recall_total_fl_random,precision_total_fl_random,color='gray',linestyle='solid',linewidth=2,label='FL_RANDOM(AUPRC=0.620)')
-plt.plot(recall_total_fl_feature,precision_total_fl_feature,color='pink',linestyle='solid',linewidth=2,label='FL_FEATURE(AUPRC=0.627)')
-plt.plot(recall_total_fl_att,precision_total_fl_att,color='purple',linestyle='solid',linewidth=2,label='FL_ATT(AUPRC=0.655)')
+plt.plot(size, train_lr, "s-",color='green', linewidth=2, linestyle='dashed',label='LR')
 
 plt.legend(loc='lower left')
 plt.show()
