306d8cf568d100a3e1dcabcc9c0c1f9a266b4155
Tingyi <tiwanyan@gmail.com>
2021-06-12 00:07:00 -0400

    correction

--- seq_cl.py
+++ seq_cl.py
@@ -632,7 +632,7 @@ class seq_cl():
             # print(j)
             self.aquire_batch_data_cl(j * self.batch_size, self.train_data, self.batch_size,
                                                 self.read_d.time_sequence)
-            self.err_ = self.sess.run([self.focal_loss, self.train_step_cl, self.logit_sig],
+            self.err_ = self.sess.run([self.focal_loss, self.train_step_combine_fl, self.logit_sig],
                                       feed_dict={self.input_x: self.one_batch_data,
                                                  self.input_y_logit: self.one_batch_logit_dp,  # })
                                                  self.input_x_pos: self.one_batch_data_pos,
@@ -647,7 +647,7 @@ class seq_cl():
             for j in range(self.iteration):
                 self.aquire_batch_data_cl_feature(j * self.batch_size, self.train_data, self.batch_size,
                                           self.read_d.time_sequence)
-                self.err_ = self.sess.run([self.focal_loss, self.train_step_cl, self.logit_sig],
+                self.err_ = self.sess.run([self.focal_loss, self.train_step_combine_fl, self.logit_sig],
                                           feed_dict={self.input_x: self.one_batch_data,
                                                      self.input_y_logit: self.one_batch_logit_dp,  # })
                                                      self.input_x_pos: self.one_batch_data_pos,
